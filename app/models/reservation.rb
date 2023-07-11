24130ee7c2b6367a587331932ac4bcf7ef0a13f4
uni1224 <saekahane99@gmail.com>
2023-07-11 08:50:15 +0000

    日時表記変更

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,11 +2,25 @@ class Reservation < ApplicationRecord
   
    belongs_to :user
   
-  # validate :date_before_start
-  # validate :date_current_today
-  # validate :date_three_month_end
+
+  #validate :date_before_start
+  #validate :date_current_today
+  #validate :date_three_month_end
+
+  def date_before_start
+    errors.add(:day, "は過去の日付は選択できません") if day < Date.current
+  end
   
- def self.check_reservation_day(day)
+  def date_current_today
+    errors.add(:day, "は当日は選択できません。予約画面から正しい日付を選択してください。") if day < (Date.current + 1)
+  end
+
+  def date_three_month_end
+    errors.add(:day, "は3ヶ月以降の日付は選択できません") if (Date.current >> 3) < day
+  end
+
+
+  def self.check_reservation_day(day)
     if day < Date.current
       return "過去の日付は選択できません。正しい日付を選択してください。"
     elsif day < (Date.current + 1)
@@ -15,16 +29,17 @@ class Reservation < ApplicationRecord
       return "3ヶ月以降の日付は選択できません。正しい日付を選択してください。"
     end
   end
-    def self.reservations_after_three_month
-    # 今日から3ヶ月先までのデータを取得
+
+
+
+  def self.reservations_after_three_month
     reservations = Reservation.all.where("day >= ?", Date.current).where("day < ?", Date.current >> 3).order(day: :desc)
-    # 配列を作成し、データを格納
-    # DBアクセスを減らすために必要なデータを配列にデータを突っ込んでます
     reservation_data = []
     reservations.each do |reservation|
-      reservations_hash = {}
-      reservations_hash.merge!(day: reservation.day.strftime("%Y-%m-%d"), time: reservation.time)
-      reservation_data.push(reservations_hash)
+    reservations_hash = {}
+    reservations_hash.merge!(day: reservation.day.strftime("%Y-%m-%d"), time: reservation.time, name: reservation.user.last_name, id:reservation.id)
+    reservation_data.push(reservations_hash)
     end
+    reservation_data
   end
 end
