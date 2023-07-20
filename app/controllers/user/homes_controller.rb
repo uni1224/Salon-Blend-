66ef518e770b884aaf571ff4ed4d30c69f9c6469
uni1224 <saekahane99@gmail.com>
2023-07-20 09:26:25 +0000

    トップページ

--- app/controllers/user/homes_controller.rb
+++ app/controllers/user/homes_controller.rb
@@ -1,5 +1,6 @@
 class User::HomesController < ApplicationController
     def top
+         @posts = Post.order("id DESC").limit(4)
     end
     def about
         @salon = Salon.find_by(params[:id])
